ffe0cb4e7bf484c1b56c5a338a52d8e25954a201
Brandon Rice <brice84@gmail.com>
2016-12-05 22:03:48 -0500

    Fixed a missing loop bug in Twitch class.

--- lib/mailbot/twitch.rb
+++ lib/mailbot/twitch.rb
@@ -26,25 +26,27 @@ module Mailbot
       initialize_channel
 
       @thread = Thread.start do
-        ready = IO.select([socket])
+        loop do
+          ready = IO.select([socket])
 
-        ready[0].each do |s|
-          line    = s.gets
-          match   = line.match(/^:(.+)!(.+) PRIVMSG #(.+) :(.+)$/)
-          message = match && match[4]
+          ready[0].each do |s|
+            line    = s.gets
+            match   = line.match(/^:(.+)!(.+) PRIVMSG #(.+) :(.+)$/)
+            message = match && match[4]
 
-          if message =~ /^!hello/
-            user = match[1]
-            logger.info "USER COMMAND: #{user} - !hello"
-            send "PRIVMSG #open_mailbox :Hello, #{user} from Mailbot!"
-          elsif message =~ /^!roll/
-            user = match[1]
-            logger.info "USER COMMAND: #{user} - !roll"
-            result = ((Random.rand * 19) + 1).round
-            send "PRIVMSG #open_mailbox :#{user} rolled 1d20 and got #{result}!"
+            if message =~ /^!hello/
+              user = match[1]
+              logger.info "USER COMMAND: #{user} - !hello"
+              send "PRIVMSG #open_mailbox :Hello, #{user} from Mailbot!"
+            elsif message =~ /^!roll/
+              user = match[1]
+              logger.info "USER COMMAND: #{user} - !roll"
+              result = ((Random.rand * 19) + 1).round
+              send "PRIVMSG #open_mailbox :#{user} rolled 1d20 and got #{result}!"
+            end
+
+            logger.info "> #{line}"
           end
-
-          logger.info "> #{line}"
         end
       end
     end
