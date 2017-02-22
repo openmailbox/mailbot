2c24467daf4cbf6c4215b150dfac626a7fe676c4
Brandon Rice <brice84@gmail.com>
2017-02-22 16:57:39 -0500

    Add basic game functionality for trivia.

--- lib/mailbot/commands/trivia.rb
+++ lib/mailbot/commands/trivia.rb
@@ -1,7 +1,10 @@
 module Mailbot
   module Commands
     class Trivia
-      attr_reader :user, :args
+      ROUND_TIME = 30 # seconds
+      BREAK_TIME = 60
+
+      attr_reader :user, :args, :context
 
       def initialize(user, args)
         @user = user
@@ -11,43 +14,96 @@ module Mailbot
       def execute(context)
         Mailbot.logger.info "USER COMMAND: #{user.name} - !trivia #{args}"
 
-        if args.first.empty?
+        @context = context
+
+        if args.empty?
           if current_game.nil?
-            context.send_string("There is no trivia game in progress.\n\tStart a new game with '!trivia start'.")
+            context.send_string("There is no trivia game in progress. Start a new game with '!trivia start'.")
           else
+            context.send_string("Trivia in progress. Currently on question #{current_game.round} of 10.")
+            ask_question
           end
         elsif args.first == 'start'
-          game = Trivia::Game.new
-          context.send_string("Starting a new trivia game!")
+          if current_game.nil?
+            context.send_string("Starting a new trivia game!")
+            @game = Trivia::Game.new
+            current_game.advance
+            ask_question
+
+            start_timer(ROUND_TIME) do
+              end_round
+            end
+          else
+            context.send_string('There is already a game in progress!')
+          end
+        elsif args.first == 'answer'
+          if args[1] =~ /\d/ && args[1].to_i <= current_game.current_choices.length
+            if current_game.answers.values.flatten.find { |i| i == user }
+              context.send_string("TRIVIA: Sorry, #{user.name}. You can't change your answer.")
+            else
+              current_game.answer(user, args[1].to_i)
+              context.send_string("TRIVIA: #{user.name}, your answer has been submitted.")
+            end
+          else
+            context.send_string("TRIVIA: Sorry, #{user.name}. That is not a valid answer choice.")
+          end
         end
       end
 
       private
 
       def answers(question)
-        choices = question['incorrect_answers'] << question['correct_answer']
-
-        choices.shuffle!
-
-        choices.map.with_index do |choice, i|
-          "#{i + 1}: #{choice}"
+        current_game.current_choices.map.with_index do |choice, i|
+          "#{i + 1}) #{choice}."
         end
       end
 
       def ask_question
-        question = current_game.current_question
+        question  = current_game.current_question
+        remaining = ROUND_TIME - (Time.now.to_i - current_game.round_started_at)
 
-        text = <<~END
-          TRIVIA QUESTION: #{question['question']}\n
-          #{answers(question).join("\n")}\n
-          Use '!trivia answer <number>' to pick an answer.
-        END
+        text  = "TRIVIA QUESTION: '#{question['question']}' --- "
+        text << "ANSWERS: '#{answers(question).join(' ')}' --- "
+        text << "Use '!trivia answer <number>' to pick an answer. "
+        text << "#{remaining} seconds remaining to submit an answer for this round."
 
         context.send_string(text)
       end
 
       def current_game
         @game ||= Trivia::Game.current
+      end
+
+      def end_round
+        text =  "TRIVIA: Round ended. "
+        text << "The correct answer was: #{current_game.current_question['correct_answer']}. "
+        text << "The following players had the correct answer: #{current_game.winners.map(&:name).join(', ')}. "
+
+        if current_game.round >= current_game.questions.length
+          winner, score = current_game.scores.sort_by { |i, j| j }.last
+
+          text << "Game over! #{winner.name} wins with a score of #{score}!"
+
+          current_game.game_over
+
+          context.send_string(text)
+        else
+          text << "The next round will start in #{BREAK_TIME} seconds."
+
+          context.send_string(text)
+
+          start_timer(BREAK_TIME) do
+            current_game.advance
+            ask_question
+          end
+        end
+      end
+
+      def start_timer(seconds, &block)
+        Thread.start do
+          sleep(seconds)
+          yield
+        end
       end
     end
   end
--- lib/mailbot/commands/trivia/game.rb
+++ lib/mailbot/commands/trivia/game.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'time'
 
 module Mailbot
   module Commands
@@ -6,7 +7,9 @@ module Mailbot
       class Game
         BASE_URL = 'https://www.opentdb.com/api.php'
 
-        attr_reader :questions
+        @@game = nil
+
+        attr_reader :questions, :current_choices, :current_question, :round, :round_started_at, :scores, :answers
 
         def self.current
           @@game
@@ -14,16 +17,45 @@ module Mailbot
 
         def initialize
           @@game     = self
-          @round     = 1
+          @round     = 0
+          @scores    = {}
           @questions = fetch_questions
+        end
+
+        def advance
+          winners.each do |user|
+            scores[user] ||= 0
+            scores[user] += 1
+          end
+
+          @answers          = {}
+          @round_started_at = Time.now.to_i
+
+          @round += 1
+
+          initialize_choices
+        end
+
+        def answer(user, choice)
+          answers[choice] ||= []
+          answers[choice] << user
         end
 
         def current_question
           questions[round - 1]
         end
 
-        def advance
-          @round += 1
+        def game_over
+          @@game = nil
+        end
+
+        def winners
+          if round > 0
+            correct_index = current_choices.index { |i| i == current_question['correct_answer'] }
+            answers[correct_index] || []
+          else
+            []
+          end
         end
 
         private
@@ -32,6 +64,12 @@ module Mailbot
           response = HTTParty.get(BASE_URL + '?amount=10')
 
           JSON.parse(response.body)['results']
+        end
+
+        def initialize_choices
+          @current_choices = current_question['incorrect_answers'] << current_question['correct_answer']
+
+          current_choices.shuffle!
         end
       end
     end
