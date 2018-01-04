f617e426940f2253f5b19ef6b03b18575e348521
open_mailbox <games@open-mailbox.com>
2018-01-04 16:22:02 -0500

    Rework chat-triggered drops to use a different console command.

--- lib/mailbot/commands/rust.rb
+++ lib/mailbot/commands/rust.rb
@@ -1,9 +1,6 @@
 module Mailbot
   module Commands
     class Rust < Mailbot::Commands::Base
-      SUPPLY_COOLDOWN = 20 # minutes
-      HELI_COOLDOWN   = 30 # minutes
-
       enable_platform :twitch
       enable_platform :discord
 
@@ -15,10 +12,8 @@ module Mailbot
         case args.first.strip
         when 'help'
           help
-        when 'supply'
-          callin('supply drop', 'supply.call', server.last_supply_at, SUPPLY_COOLDOWN)
-        when 'heli'
-          callin('attack helicopter', 'heli.call', server.last_heli_at, HELI_COOLDOWN)
+        when 'drop'
+          Rust::Drop.new(self).execute
         else
           "Unrecognized command. Try '!rust help'."
         end
@@ -30,28 +25,10 @@ module Mailbot
 
       private
 
-      def callin(name, command, last, cooldown)
-        expires_at = last.to_i + (cooldown * 60)
-        attribute  = command == 'supply.call' ? :last_supply_at : :last_heli_at
-
-        if DateTime.now.to_i >= expires_at
-          server.rcon(command, blocking: false)
-          server.update_attributes(attribute => DateTime.now)
-          "Calling in the #{name}!"
-        else
-          remaining = expires_at - DateTime.now.to_i
-          minutes   = remaining / 60
-          seconds   = remaining % 60
-
-          "Cooldown active. Please wait #{minutes} minutes, #{seconds} seconds."
-        end
-      end
-
       def help
         text = 'Available Rust commands: '
         text << "'!rust' shows the current server status. "
-        text << "'!rust supply' calls in a supply drop (#{SUPPLY_COOLDOWN} min cooldown)."
-        text << "'!rust heli' calls in an attack helicopter (#{HELI_COOLDOWN} min cooldown)."
+        text << "'!rust drop' calls in a supply drop (#{Rust::Drop::COOLDOWN} min cooldown)."
       end
     end
   end
---
+++ lib/mailbot/commands/rust/drop.rb
@@ -0,0 +1,31 @@
+module Mailbot
+  module Commands
+    class Rust
+      class Drop
+        COOLDOWN = 20 # minutes
+
+        attr_reader :response, :rust
+
+        def initialize(rust)
+          @rust = rust
+        end
+
+        def execute
+          expires_at = rust.last_supply_at.to_i + (COOLDOWN * 60)
+
+          if DateTime.now.to_i >= expires_at
+            @response   = rust.server.rcon('airdrop random')
+            coordinates = response['Message'].match(/\(.+\)/)[0]
+            "#{user.name} has called in an air drop at #{coordinates}."
+          else
+            remaining = expires_at - DateTime.now.to_i
+            minutes   = remaining / 60
+            seconds   = remaining % 60
+
+            "Cooldown active. Please wait #{minutes} minutes, #{seconds} seconds."
+          end
+        end
+      end
+    end
+  end
+end
