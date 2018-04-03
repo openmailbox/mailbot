3355884504c869cb9e1762c693531a1b3cc53a2f
Brandon <brandon@blrice.net>
2018-04-03 00:43:48 -0400

    Create Steam + Newegg feeds in db/seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 # The Kadgar job maintains a up-to-date list of live streamers in a specific Discord channel.
 #details = { discord_channel_id: '12345', twitch_ids: %w{678 901} }
 #Mailbot::Models::Kadgar.create!(frequency: 1800, details: details)
+
+steam = NewsFeed.find_or_create_by(link: 'http://store.steampowered.com/feeds/news.xml')
+newegg = NewsFeed.find_or_create_by(link: 'https://www.newegg.com/Product/RSS.aspx?Submit=RSSDailyDeals&Depa=0')
--- lib/mailbot/models/news_feed.rb
+++ lib/mailbot/models/news_feed.rb
@@ -1,5 +1,6 @@
 module Mailbot
   module Models
+    # TODO: Update NewsFeed fields with RSS-provided data
     class NewsFeed < ActiveRecord::Base
       has_many :rss_items
     end
