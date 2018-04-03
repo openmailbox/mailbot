cdf1329522fb69b780cf888ee3d6e844a21d89a0
Brandon <brandon@blrice.net>
2018-04-03 00:11:18 -0400

    Refactor RSS feed fetching and item creation.

--- lib/mailbot/models/jobs/update_news_feed.rb
+++ lib/mailbot/models/jobs/update_news_feed.rb
@@ -4,6 +4,18 @@ module Mailbot
       def perform
         self.last_run_at = DateTime.now.utc
 
+        new_items = find_or_create_items!
+
+        save!
+      end
+
+      private
+
+      # Fetch the RSS feed and save any new items
+      #
+      # @return [Mailbot::Models::RssItem] The newly created RSS items
+      def find_or_create_items!
+        new_items = []
         news_feed = NewsFeed.find_by(id: details['news_feed_id'])
         reader    = news_feed&.reader_class&.constantize&.new
 
@@ -19,13 +31,13 @@ module Mailbot
 
           next if existing
 
-          news_feed.rss_items.create!(guid:         item.guid,
-                                      title:        item.title,
-                                      published_at: item.published_at,
-                                      description:  item.description)
+          new_items << news_feed.rss_items.create!(guid:         item.guid,
+                                                   title:        item.title,
+                                                   published_at: item.published_at,
+                                                   description:  item.description)
         end
 
-        save!
+        new_items
       end
     end
   end
