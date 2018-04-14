b8e24fdcd62cd818065437f27a35aa400ec1cc16
Brandon <brandon@blrice.net>
2018-04-14 00:24:48 -0400

    Update seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,4 @@
 steam = Mailbot::Models::NewsFeed.find_or_create_by(link: 'http://store.steampowered.com/feeds/news.xml', reader_class: 'Mailbot::RSS::Steam')
 newegg = Mailbot::Models::NewsFeed.find_or_create_by(link: 'https://www.newegg.com/Product/RSS.aspx?Submit=RSSDailyDeals&Depa=0', reader_class: 'Mailbot::RSS::Newegg')
 humble = Mailbot::Models::NewsFeed.find_or_create_by(link: 'http://blog.humblebundle.com/rss', reader_class: 'Mailbot::RSS::HumbleBundle')
+blizzard = Mailbot::Models::NewsFeed.find_or_create_by(link: 'https://news.blizzard.com/en-us', reader_class: 'Mailbot::RSS::Blizzard')
