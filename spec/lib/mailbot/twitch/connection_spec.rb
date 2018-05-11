3b7e42a61e5d6ed5ea3080acbd650576046a4559
Brandon R <brice84@gmail.com>
2018-05-11 16:02:10 -0400

    WIP Tests for sockets.

---
+++ spec/lib/mailbot/twitch/connection_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe Mailbot::Twitch::Connection do
+  #let(:mock_socket) { StringIO.new }
+
+  #before(:each) do
+  #  allow(TCPSocket).to receive(:new).and_return(mock_socket)
+
+  #  allow(IO).to receive(:select) do
+  #    [[], [], [], 42]
+  #  end
+  #end
+
+  #it_behaves_like 'a Connection'
+end
