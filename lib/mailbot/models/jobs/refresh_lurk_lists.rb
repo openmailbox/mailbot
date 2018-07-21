f862686875345f33f6756724856772a6358d11a3
Brandon <brandon@blrice.net>
2018-07-21 16:47:23 -0400

    Implement creation of new LurkLists from the web API.

--- lib/mailbot/models/jobs/refresh_lurk_lists.rb
+++ lib/mailbot/models/jobs/refresh_lurk_lists.rb
@@ -2,8 +2,27 @@ module Mailbot
   module Models
     class RefreshLurkLists < Job
       def perform
-        self.last_run_at = Time.now.utc
+        last = last_run_at.to_i
+        self.last_run_at = DateTime.now.utc
+
+        api.lurk_lists(last).each do |data|
+          list = LurkList.find_or_initialize_by(mailbot_rails_id: data['id'])
+
+          list.nickname           = data['nickname']
+          list.twitch_names       = data['twitch_names']
+          list.discord_channel_id = data['discord_channel_id']
+          list.guild_id           = data['guild_id']
+
+          list.save! if list.changed?
+        end
+
         save!
+      end
+
+      private
+
+      def api
+        @api ||= Mailbot::WebClient.new
       end
     end
   end
--- spec/lib/mailbot/models/jobs/refresh_lurk_lists_spec.rb
+++ spec/lib/mailbot/models/jobs/refresh_lurk_lists_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Mailbot::Models::RefreshLurkLists do
   end
 
   it_behaves_like 'a scheduled job', { frequency: 42 }
+
+  after(:each) { Mailbot::Models::LurkList.destroy_all }
+
+  subject(:job) { described_class.new(frequency: 42) }
+
+  it 'creates lurk lists' do
+    expect {
+      job.perform
+    }.to change { Mailbot::Models::LurkList.count }.by(2)
+  end
+
+  it 'removes lurk lists'
 end
