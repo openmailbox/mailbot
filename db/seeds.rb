0fe4962e29d6efe7d7ddb98f378ed9dbddb3ba67
open_mailbox <games@open-mailbox.com>
2018-10-05 15:57:51 -0400

    Remove deprecated Kadgar job.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,6 @@
 # Add channels to create a Twitch chat connection for each.
 # Mailbot::Models::Channel.find_or_create_by(name: 'my_twitch_channel')
 
-# The Kadgar job maintains a up-to-date list of live streamers in a specific Discord channel.
-#details = { discord_channel_id: '12345', twitch_ids: %w{678 901} }
-#Mailbot::Models::Kadgar.create!(frequency: 1800, details: details)
-
 steam = Mailbot::Models::NewsFeed.find_or_create_by(link: 'http://store.steampowered.com/feeds/news.xml', reader_class: 'Mailbot::RSS::Steam')
 newegg = Mailbot::Models::NewsFeed.find_or_create_by(link: 'https://www.newegg.com/Product/RSS.aspx?Submit=RSSDailyDeals&Depa=0', reader_class: 'Mailbot::RSS::Newegg')
 humble = Mailbot::Models::NewsFeed.find_or_create_by(link: 'http://blog.humblebundle.com/rss', reader_class: 'Mailbot::RSS::HumbleBundle')
